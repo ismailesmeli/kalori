593c6ecaaeaaec1fe542ad2bae9106fae40943cd
ismailesmeli <ismailesmeli048@gmail.com>
2026-04-16 23:21:08 +0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,12 @@
-var CACHE_NAME = 'nutritrack-v1';
 var CACHE_NAME = 'saglicakla-v1';
 var urlsToCache = [
     './index.html',
     './manifest.json',
-    'https://cdn.jsdelivr.net/npm/chart.js@4.4.0/dist/chart.umd.min.js'
+    'https://cdn.jsdelivr.net/npm/chart.js@4.4.0/dist/chart.umd.min.js',
+    // Manifest dosyanızda belirttiğiniz ikonların yollarını buraya ekleyin.
+    // Örnek:
+    './icon-192x192.png',
+    './icon-512x512.png'
 ];
 
 self.addEventListener('install', function(event) {
